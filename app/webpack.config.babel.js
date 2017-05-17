ea40e795ff6c7dc69577b4aaff6ae070ea9cfa3f
Robert Smith <rbrtsmith84@googlemail.com>
2017-05-17 23:08:07 +0100

    Basic webpack / react setup

---
+++ app/webpack.config.babel.js
@@ -0,0 +1,14 @@
+import path from 'path';
+
+export default {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      { test: /\.jsx?$/, use: 'babel-loader' }
+    ]
+  }
+};
