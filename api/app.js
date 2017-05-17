b8e26ff3330f6d61d209551426ce4d559b7d6b6c
Robert Smith <rbrtsmith84@googlemail.com>
2017-05-17 22:09:23 +0100

    Setup inital route and handle 404s

--- api/app.js
+++ api/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 require('dotenv').config();
+const routes = require('./routes');
+const { notFound } = require('./handlers');
 
 const app = express();
 
+app.use('/', routes);
+app.use(notFound);
+
 const PORT = process.env.PORT || 9999;
-app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
---
+++ api/handlers.js
@@ -0,0 +1,4 @@
+exports.notFound = (req, res, next) => {
+  res.status(404).send('404 not found!');
+  next();
+};
