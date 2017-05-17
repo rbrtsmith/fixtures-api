37e8f6ef441f61c0d025c01c7b760d63f7d4b0ea
Robert Smith <rbrtsmith84@googlemail.com>
2017-05-17 22:36:47 +0100

    generate unique list of areas

--- api/seedDB.js
+++ api/seedDB.js
@@ -3,7 +3,24 @@ const { parseString } = require('xml2js');
 require('dotenv').config();
 
 function transformAndInsertData(err, data) {
-  console.log(data)
+  if (err) {
+    console.log('Unable to parse XML');
+    process.exit();
+  }
+
+  const areas = [];
+  const competitions = [];
+  const matches = [];
+
+  data.gsmrs.competition.forEach((competition) => {
+    if(!areas.find(item => item.id === competition.$.area_id)) {
+      areas.push({
+        id: competition.$.area_id,
+        name: competition.$.area_name
+      })
+    }
+  })
+  console.log(areas)
 }
 
 fetch(process.env.DATA_URL)
