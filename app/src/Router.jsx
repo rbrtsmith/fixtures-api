7e5afd889a54429171682c71e9997eec00795b02
Robert Smith <rbrtsmith84@googlemail.com>
2017-05-18 18:46:15 +0100

    Add routing and redux flow for Areas and Competitions

--- app/src/Router.jsx
+++ app/src/Router.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import AreasListContainer from './AreasListContainer';
-import AreaContainer from './AreaContainer';
+import AreasListContainer from './modules/areas/ListContainer';
+import CompetitionListContainer from './modules/competitions/ListContainer';
 import MatchListContainer from './MatchListContainer';
 
 export default () => (
   <Router>
     <div>
+      <h1><Link to="/">Fixtures around the world</Link></h1>
       <Route exact path="/" component={AreasListContainer} />
-      <Route path="/areas/:id/competitions" component={AreaContainer} />
+      <Route path="/areas/:id/competitions" component={CompetitionListContainer} />
       <Route path="/competitions/:id/matches" component={MatchListContainer} />
     </div>
   </Router>
--- app/src/store/index.js
+++ app/src/store/index.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 
-import rootReducer from './rootReducer';
+import rootReducer from './reducers/rootReducer';
 
 const middleware = compose(
   applyMiddleware(thunk),
